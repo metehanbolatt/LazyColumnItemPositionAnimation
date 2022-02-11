f8140f064a49ceab108ed1a776f0808bba7a51eb
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-02-11 16:36:56 +0300

    Compose Version updated to 1.1.0

--- app/src/main/java/com/metehanbolat/lazycolumnitempositionanimation/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/lazycolumnitempositionanimation/MainActivity.kt
@@ -3,14 +3,49 @@ package com.metehanbolat.lazycolumnitempositionanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.metehanbolat.lazycolumnitempositionanimation.ui.theme.LazyColumnItemPositionAnimationTheme
 
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LazyColumnItemPositionAnimationTheme {
-                
+                var items by remember {
+                    mutableStateOf(
+                        listOf(
+                            "Kotlin",
+                            "Java",
+                            "Python",
+                            "GO",
+                            "C++"
+                        )
+                    )
+                }
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+
+                }
             }
         }
     }
